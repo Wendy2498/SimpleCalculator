531ef1fbfda4a3e28f6313209ed3539a3ca884ca
Wendy2498 <WFAULK2498@smail.pcd.edu>
2021-05-24 20:02:56 -0700

    Add project files.

---
+++ SimpleCalculator/SimpleCalculator.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace SimpleCalculator
+{
+    partial class FrmSimpleCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperand1 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.txtOperator1 = new System.Windows.Forms.TextBox();
+            this.lblOperand2 = new System.Windows.Forms.Label();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperand1
+            // 
+            this.lblOperand1.AutoSize = true;
+            this.lblOperand1.Location = new System.Drawing.Point(45, 30);
+            this.lblOperand1.Name = "lblOperand1";
+            this.lblOperand1.Size = new System.Drawing.Size(65, 15);
+            this.lblOperand1.TabIndex = 0;
+            this.lblOperand1.Text = "Operand 1:";
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.AcceptsTab = true;
+            this.txtOperand1.Location = new System.Drawing.Point(144, 21);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 23);
+            this.txtOperand1.TabIndex = 0;
+            this.txtOperand1.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(45, 66);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(57, 15);
+            this.lblOperator.TabIndex = 0;
+            this.lblOperator.Text = "Operator:";
+            // 
+            // txtOperator1
+            // 
+            this.txtOperator1.AcceptsTab = true;
+            this.txtOperator1.Location = new System.Drawing.Point(144, 58);
+            this.txtOperator1.Name = "txtOperator1";
+            this.txtOperator1.Size = new System.Drawing.Size(51, 23);
+            this.txtOperator1.TabIndex = 1;
+            this.txtOperator1.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // lblOperand2
+            // 
+            this.lblOperand2.AutoSize = true;
+            this.lblOperand2.Location = new System.Drawing.Point(45, 105);
+            this.lblOperand2.Name = "lblOperand2";
+            this.lblOperand2.Size = new System.Drawing.Size(65, 15);
+            this.lblOperand2.TabIndex = 0;
+            this.lblOperand2.Text = "Operand 2:";
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.AcceptsTab = true;
+            this.txtOperand2.Location = new System.Drawing.Point(144, 97);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 23);
+            this.txtOperand2.TabIndex = 2;
+            this.txtOperand2.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(45, 144);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(42, 15);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.AcceptsTab = true;
+            this.txtResult.Location = new System.Drawing.Point(144, 136);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 23);
+            this.txtResult.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(61, 173);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(171, 173);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(73, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmSimpleCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(316, 248);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.lblOperand2);
+            this.Controls.Add(this.txtOperator1);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.lblOperand1);
+            this.Name = "FrmSimpleCalculator";
+            this.Text = "SimpleCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperand1;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.TextBox txtOperator1;
+        private System.Windows.Forms.Label lblOperand2;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,81 @@
+﻿/*Wendy Faulk
+ * 5/20/2021
+ * CIS 123
+ * Week 4 Murach Coding Assignmnet (Teamwork--Done Individually)
+ * Extra 6-1 Create a simple calculator */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class FrmSimpleCalculator : Form
+    {
+        public FrmSimpleCalculator()
+        {
+            InitializeComponent();
+        }
+
+        //Step 3. Create method "Calculate" to calculate the Result value,
+        //using Operand1, Operator and Operand2 as parameters.
+        private decimal Calculate(decimal operand1, string operator1, decimal operand2)
+        {
+            //Set Result value to 0, will take various calculation values.
+            decimal result = 0m;
+
+            //Use if else to calculate result based upon Operator entered.
+            if (operator1 == "+")
+                result = operand1 + operand2;
+            else if (operator1 == "-")
+                result = operand1 - operand2;
+            else if (operator1 == "*")
+                result = operand1 * operand2;
+            else if (operator1 == "/")
+                result = operand1 / operand2;
+            return result;               //Return the calculated result of this called method.
+        }
+       
+
+        //Step 4. Create Calculate Click event handler.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Get the Operand 1 text value and convert it to a decimal.
+            decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
+
+            //Convert the Operator text value to a string.
+             string operator1 = Convert.ToString(txtOperator1.Text);
+
+            //Get the Operand 2 text value and convert it to a decimal.
+            decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
+
+            //Call Calculate method, pass 3 arguments.
+            decimal result = Calculate(operand1, operator1, operand2);
+
+            //Display result in form, rounded to 4 places.
+            txtResult.Text = Math.Round(result, 4).ToString();
+
+            //Move focus to Operand 1 text box
+            txtOperand1.Focus();
+        }
+
+        //Create event handler for Exit button that closes the form.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Create event handler that clears Result text box when user changes text in another box.
+        private void ClearResult(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+    }
+}
+
